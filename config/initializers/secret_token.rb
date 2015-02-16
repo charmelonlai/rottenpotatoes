42ca7db8d0ce50e104ca1a7092237964dec7867b
saasbook <saasbook@saasbook.(none)>
2015-02-16 05:06:42 -0400

    first deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '4fc1e9791443bbe71683a1a7de3700862c6f1c760586cec64699df7b65a9dc4729f15c4be46c7fbcc1ddce94e117b4c3325d09483f46759c8b3d45800f3f963f'
